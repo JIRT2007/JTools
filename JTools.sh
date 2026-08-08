4623c853d60de9feadf22923e1dfdd8cba502794
JIRT2007 <juanignaciorios2007@gmail.com>
2026-08-08 19:52:52 -0300

    v0.0.1

---
+++ JTools.sh
@@ -0,0 +1,210 @@
+#!/bin/bash
+
+####################
+### Notas        ###
+####################
+    # BOLD TEXT ANSI: \e[1m . . . \e[0m
+        ## Dentro del EOF se precisa previamente declarar la expansion de variables
+            ### BOLD="\e[1m"
+            ### RESET="\e[0m"
+
+    # ESTRUCTURAS UNICODE: ╔ ╗ ╚ ╝ ║ ═ ╠ ╣
+
+####################
+### Variables    ###
+####################
+VM_DIR="$HOME/JTools/.qemu-vm"
+CONFIG_DIR="$VM_DIR/config"
+DISK_DIR="$VM_DIR/disks"
+
+
+####################
+### Ver VMs      ###
+####################
+viewVM(){
+    clear
+    echo ""
+    echo -e " \e[1m   Stored Virtual Machines: \e[0m"
+            cat "$CONFIG_DIR"/*.cfg
+    echo ""    
+    read -n1 -s -r -p "    Press any key to continue..."    
+        return   
+}
+
+####################
+### Eliminar VMs ###
+####################
+deleteVM(){
+    echo "    Existing VMs configurations:"
+	echo ""
+    ls "$CONFIG_DIR"/*.cfg | xargs -n1 basename
+    	echo ""  
+    read -e -p "    Enter the name of the VM configuration: " vmDEL
+
+        if [ -f "$CONFIG_DIR/$vmDEL" ]; then # -f significa "¿Existe el archivo guardado en la variable $vm?"
+            source "$CONFIG_DIR/$vmDEL"
+                rm "$CONFIG_DIR/$vmDEL"
+                rm "$DISK"
+        else 
+            echo " Virtual machine does not exist."
+        fi
+}
+
+####################
+### Iniciar VMs  ###
+####################
+startVM(){
+    echo "    Existing VMs configurations:"
+    echo ""
+        ls "$CONFIG_DIR"/*.cfg | xargs -n1 basename  
+    echo ""  
+    read -e -p "    Enter the name of the VM configuration: " vm   
+    
+        if [ -f "$CONFIG_DIR/$vm" ]; then # -f significa "¿Existe el archivo guardado en la variable $vm?"
+            source "$CONFIG_DIR/$vm" #source es un comando de Bash que lee y ejecuta el contenido de un archivo en la shell actual.
+                qemu-system-x86_64 -m $RAM -smp $CPU --enable-kvm -drive file=$DISK,format=qcow2 -nic user,model=e1000,hostfwd=tcp::2222-:22 &
+        else 
+            echo " Virtual machine does not exist."
+        fi
+}
+
+####################
+### Crear VMs    ###
+####################
+createVM(){
+    read -p "    VM name: " nameVM
+    read -e -p "    Disk name: " name
+    read -p "    Disk size (GB): " size
+        disk="$VM_DIR/disks/${name}.qcow2"
+            qemu-img create -f qcow2 "$disk" "${size}G"
+
+    read -p "    RAM (MB): " ram
+    read -p "    CPUs: " cpu
+    read -e -p "    ISO location: " iso
+        qemu-system-x86_64 -m "$ram" -smp "$cpu" --enable-kvm -boot d -cdrom "$iso" -drive file="$disk",format=qcow2 -nic user,model=e1000 &
+
+cat > "$VM_DIR/config/${nameVM}.cfg" << EOF
+    
+ NAME=$nameVM   
+ RAM=$ram       
+ CPU=$cpu       
+ ISO=$iso       
+ DISK=$disk         
+EOF
+    
+}
+
+#################
+### Menu QEMU ###
+#################
+QEMU_MENU(){
+while true; do
+    clear
+
+cat << "EOF"
+
+    ██████    ██████████ ██████   ██████ █████  █████
+  ███▒▒▒▒███ ▒▒███▒▒▒▒▒█▒▒██████ ██████ ▒▒███  ▒▒███ 
+ ███    ▒▒███ ▒███  █ ▒  ▒███▒█████▒███  ▒███   ▒███ 
+▒███     ▒███ ▒██████    ▒███▒▒███ ▒███  ▒███   ▒███ 
+▒███   ██▒███ ▒███▒▒█    ▒███ ▒▒▒  ▒███  ▒███   ▒███ 
+▒▒███ ▒▒████  ▒███ ▒   █ ▒███      ▒███  ▒███   ▒███ 
+ ▒▒▒██████▒██ ██████████ █████     █████ ▒▒████████  
+   ▒▒▒▒▒▒ ▒▒ ▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒     ▒▒▒▒▒   ▒▒▒▒▒▒▒▒   
+═════════════════════════════════════════════════════════
+    [1] Create virtual machine.
+    [2] Start virtual machine.
+    [3] Delete virtual machine.
+    [4] Stored virtual machines.
+    [0] Exit.
+═════════════════════════════════════════════════════════
+EOF
+
+    read -p "    Enter your order: " optionMAIN_QEMU
+    case $optionMAIN_QEMU in 
+
+        1)
+            createVM
+        ;;
+
+        2)
+            startVM
+        ;;
+
+        3)
+            deleteVM
+        ;;
+
+        4)
+            viewVM
+        ;;
+
+        0)
+            clear
+            return
+        ;;
+
+        *)
+            echo "Order not found."
+            sleep 2
+        ;;
+
+esac
+done
+}
+
+####################
+### Menu inicial ###
+####################
+while true; do
+    clear 
+
+cat << "EOF"
+
+       █████ ███████████                   ████         
+      ▒▒███ ▒█▒▒▒███▒▒▒█                  ▒▒███         
+       ▒███ ▒   ▒███  ▒   ██████   ██████  ▒███   █████ 
+       ▒███     ▒███     ███▒▒███ ███▒▒███ ▒███  ███▒▒  
+       ▒███     ▒███    ▒███ ▒███▒███ ▒███ ▒███ ▒▒█████ 
+ ███   ▒███     ▒███    ▒███ ▒███▒███ ▒███ ▒███  ▒▒▒▒███
+▒▒████████      █████   ▒▒██████ ▒▒██████  █████ ██████ 
+ ▒▒▒▒▒▒▒▒      ▒▒▒▒▒     ▒▒▒▒▒▒   ▒▒▒▒▒▒  ▒▒▒▒▒ ▒▒▒▒▒▒  
+═════════════════════════════════════════════════════════
+    [1] Install programs.
+    [2] KVM/QEMU manager.
+    [3] Firewall UFW.
+    [4] Docker compose.
+    [0] Exit.
+═════════════════════════════════════════════════════════
+EOF
+    read -p "    Enter your order: " optionMAIN
+    case $optionMAIN in
+        
+        1)
+        
+        ;;
+
+        2)
+            QEMU_MENU
+        ;;
+
+        3)
+
+        ;;
+
+        4)
+
+        ;;
+
+        0)
+            clear
+            exit 0
+        ;;
+
+        *)
+            echo "Order not found."
+            sleep 2
+        ;;
+
+esac
+done
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # JTools
-GNU/Linux toolkit developed by JIRT2007
+**JTools** es un simple conjunto de herramientas de terminal desarrollado por **JIRT2007** con el objetivo de ofrecer a desarrolladores y estudiantes la posibilidad de simplificar el manejo de herramientas del ecosistema GNU/Linux mediante la ejecución de un simple script de shell. El mismo código esta desarrollado en Shell Script con la finalidad de priorizar la eficiencia, estabilidad y la auditabilidad, se destina su uso esencialmente a sistemas del ecosistema **Debian GNU/Linux** y derivados (Ubuntu, LinuxMint, ZorinOS, Devuan, etc).
+
+## Características:
+- JTools se encuentra programado íntegramente en el lenguaje del paradigma imperativo **Bash Script** y se define enteramente en un único archivo de programación, esto con el objetivo de simplificar su manejo y no mantener varios scripts que puedan dificultar el trabajo tanto de los desarrolladores como de los usuarios. JTools adopta, por filosofía del desarrollador un diseño monolítico (Un único archivo de script que define toda la lógica de programación).
+- JTools ofrece diferentes funciones que son primordiales para el trabajo diarios de un desarrollador o estudiante como lo seria la creacion básica de maquinas virtuales, el manejo de contenedores mediante **Docker** o la gestión del firewall **UFW** (incorporado en muchas distribuciones de GNU/Linux y sus repositorios) entre muchas otras funciones que se implementaran.
+- JTools es un proyecto de carácter académico y no busca la creación de nuevas tecnologías, simplemente facilitar el manejo de las mismas que implementa.
+- Se recomienda su uso para sistemas operativos **Debian GNU/Linux** y derivados con arquitectura **x86_64** y **BASH** como shell del sistema (se puede saber cual es la que ofrece su sistema con el comando `echo $SHELL`. Esto no implica que sea estricto su uso para estos sistemas, puede ser utilizado para sistemas base **RHEL/Fedora** y **ArchLinux** pero se recomienda su uso en sistemas del ecosistema **Debian**.
+
+## Instalación y recomendaciones: 
+### Clonado del repositorio y alias:
+- Instalar el programa GIT:         `sudo apt install git`
+- Configurar usuario:               `git config --global user.name "Nombre de usuario de GIT"`
+- Configurar correo electrónico:    `git config --global user.email "Correo de GIT"`
+- Clonar repositorio:               `git clone https://github.com/JIRT2007/JTools.git`
+
+Se recomienda para facilitar el uso del script el convertirlo en un alias para poder ejecutarlo como comando en su respectiva terminal de comandos. Esto se puede hacer editando el archivo `.bashrc` y colocar al final del mismo un alias del estilo: 
+    `alias jtools='bash ~/JTools/JTools.sh'`
+Luego se deberá de recargar la configuración del mismo mediante el comando `source .bashrc`. Esto permitirá que al ejecutar el comando `jtools` en la terminal se inicie el script.
+
+### Se recomienda tener instalados los siguientes programas y servicios:
+- Docker.
+- KVM/QEMU.
+- UFW.
+
+## Licencias y alojamiento:
+El código de **JTools** se encuentra actualmente alojado dentro de un repositorio de la plataforma GitHub y se respalda tras la certificación de **General Public License v2.0** (**GPL v2.0**) cuyas especificaciones podrán encontrarse en la respectiva web del proyecto **GNU**.
+
+**https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html**
+
+## Autor: 
+**JIRT2007** es el desarrollador y mantenedor principal del proyecto **JTools** al igual que de proyectos como **CentrixCL** del cual se podrá encontrar información en su respectiva web alojada en GitHub Pages: **https://jirt2007.github.io/webCENTRIX/**
+
+## Changelog:
+- **v0.0.1 :** Se ofrece la primera versión oficial de JTools, el mismo ofrece unicamente un gestor de VMs mediante KVM/QEMU. Futuras funcionalidades serán implementadas a futuro. Muchas gracias :)
